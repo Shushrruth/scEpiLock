aed75977d6d8f0d686fbd929ed394ca7d740577c
Shushrruth Sai <shushrruthsai@gmail.com>
2022-04-12 16:17:56 -0700

    inital

---
+++ Test/tester.py
@@ -0,0 +1,17 @@
+import pandas as pandas
+import numpy as np
+
+data_dir = "/gpfs/gibbs/pi/girgenti/JZhang/commonData/scEpiLock/traning"
+
+ATAC_labels = pd.read_csv(data_dir + "y-labels-positives.for-trainging.txt", header = 0, sep ='\t')
+
+cell_types  = ["Ast", "End", "Ex" , "In" , "Mic" , "Oli", "OPC"]
+
+ct = []
+
+for cell_type in cell_types:
+
+	ct.append (sum(ATAC_labels[cell_type].values)
+
+
+print(ct)
