1eabfab6eadd36c9f6dc3cd7225b6734e8620096
Shushrruth Sai <shushrruthsai@gmail.com>
2022-04-14 22:03:16 -0700

    initial

--- submit/submit_Test.sh
+++ submit/submit_Test.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name=test
 #SBATCH --time=1-
 #SBATCH --gpus 1
-#SBATCH --cpus-per-gpu 8
+#SBATCH --cpus-per-task 8
 #SBATCH --mem-per-cpu 5g
 #SBATCH --ntasks=1 
 #SBATCH --partition=gpu
 #
 module load miniconda
 conda activate ScEpilock
-python Test/tester.py
+python3 Test/tester.py
 
 
 
