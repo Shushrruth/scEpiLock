ec37822d0cf6b233618f13ec8f3372f4f09e8aa6
shushrruth <shushrruthsai@gmail.com>
2022-04-19 00:37:05 -0500

    init

--- binary_classifier/__main__.py
+++ binary_classifier/__main__.py
@@ -14,11 +14,10 @@ data_train, data_eval, data_test, label_train, label_eval, label_test = data_cla
 
 # Loading and one-hot-encoding
 
-#train_data_loader = DataLoader(data_train, label_train)
-#eval_data_loader = DataLoader(data_eval, label_eval)
+train_data_loader = DataLoader(data_train, label_train)
+eval_data_loader = DataLoader(data_eval, label_eval)
 
-train_data_loader = DataLoader(data_train[:640], label_train[:640])
-eval_data_loader = DataLoader(data_eval[:64], label_eval[:64])
+
 
 
 # Trainer
@@ -26,7 +25,7 @@ eval_data_loader = DataLoader(data_eval[:64], label_eval[:64])
 model_wt_path = "/home/sss253/project/scEpiLock_weights/04-18-model_v2.pt"
 epochs = 25
 batch_size = 64
-learning_rate = 5e-4
+learning_rate = 1e-3
 weight_decay = 5e-4
 n_class = 7
 
