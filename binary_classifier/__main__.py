df99cec625a54c5490736aa6002d2e88ade3e6cc
shushrruth <shushrruthsai@gmail.com>
2022-04-19 00:28:43 -0500

    init

--- binary_classifier/__main__.py
+++ binary_classifier/__main__.py
@@ -14,9 +14,11 @@ data_train, data_eval, data_test, label_train, label_eval, label_test = data_cla
 
 # Loading and one-hot-encoding
 
-train_data_loader = DataLoader(data_train, label_train)[:100]
-eval_data_loader = DataLoader(data_eval, label_eval)
+#train_data_loader = DataLoader(data_train, label_train)
+#eval_data_loader = DataLoader(data_eval, label_eval)
 
+train_data_loader = DataLoader(data_train[:640], label_train[:640])
+eval_data_loader = DataLoader(data_eval[:64], label_eval[:64])
 
 
 # Trainer
@@ -28,7 +30,7 @@ learning_rate = 5e-4
 weight_decay = 5e-4
 n_class = 7
 
-trainer = Trainer(train_data_loader, eval_data_loader, model_wt_path, epochs, batch_size, 
+trainer = Trainer(train_data_loader[:100], eval_data_loader, model_wt_path, epochs, batch_size, 
 	learning_rate, weight_decay, '_' , '/', n_class,scEpiLock)
 
 
