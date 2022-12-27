b953a4421df7584b11a49aec0094d5dc0df14833
Shushrruth Srinivasan <shushrrs@galaxy.ics.uci.edu>
2022-12-27 14:24:40 -0800

    result

--- multi_task/data/data_processor.py
+++ multi_task/data/data_processor.py
@@ -7,7 +7,7 @@ class PreProcessor():
     def __init__(self, data_dir):
 
         self.pos_fasta_path = data_dir + "union_peaks_1000.fa" # X-centered pos
-        #self.neg_fasta_path = data_dir + "x-centered-negatives-ENCODE-5000bpAway.fa" 
+        self.neg_fasta_path = data_dir + "x-centered-negatives-Random-2000bpAway.fa" 
         self.label_path = data_dir + "new_lables.tsv" # y-labels
 
     def get_ratio(self,df):
@@ -22,30 +22,30 @@ class PreProcessor():
     def concat_data(self):
 
         pos_fasta = pd.read_csv(self.pos_fasta_path,sep=">chr*",header=None, engine='python').values[1::2][:,0] 
-        #neg_fasta = pd.read_csv(self.neg_fasta_path,sep=">chr*",header=None, engine='python').values[1::2][:,0] 
+        neg_fasta = pd.read_csv(self.neg_fasta_path,sep=">chr*",header=None, engine='python').values[1::2][:,0] 
 
         label = pd.read_csv(self.label_path, sep='\t', header=0)
 
         pos_label = label.to_numpy()
 
-        train_weights = self.get_ratio(label)
+        #train_weights = self.get_ratio(label)
 
-        #len_p = round(len(pos_fasta)/5)
+        len_p = round(len(pos_fasta))
         
-        #np.random.seed(42)
-        #np.random.shuffle(neg_fasta)
-        #neg_fasta = neg_fasta[:len_p]
+        np.random.seed(42)
+        np.random.shuffle(neg_fasta)
+        neg_fasta = neg_fasta[:len_p]
 
 
 
-        #neg_label = np.zeros((len_p, 7))
+        neg_label = np.zeros((len_p, 7))
 
         #print(pos_label.shape)
         #print(neg_label.shape)
                 
-        #data_1 = np.concatenate([pos_fasta,neg_fasta])
-        #label_1 = np.concatenate([pos_label,neg_label])
-        
+        data_1 = np.concatenate([pos_fasta,neg_fasta])
+        label_1 = np.concatenate([pos_label,neg_label])
+        train_weights = self.get_ratio(pd.DataFrame(label_1))
 
         #print("positive size = ", pos_fasta.shape)  
         #print("positive label size = ",pos_label.shape) 
@@ -54,7 +54,7 @@ class PreProcessor():
         #print("Total label size = ", neg_label.shape)
 
 
-        return pos_fasta, pos_label, train_weights
+        return data_1, label_1, train_weights
 
     def split_train_test(self, data, label, test_size = 0.1):
         data_train_temp, data_test, label_train_temp, label_test = train_test_split(data, label, test_size=test_size, random_state=12)
--- submit/submit.sh
+++ submit/submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Tuned_model
+#SBATCH --job-name=Tuned_model_test
 #SBATCH --time=2-
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
@@ -7,10 +7,10 @@
 #SBATCH --mem 16G
 #SBATCH --ntasks=1 
 #SBATCH --partition=zhanglab.p
-#SBATCH --out=out/tuned_model.o
+#SBATCH --out=out/tuned_model_2_test.o
 
 
 module load anaconda
 source /pkg/anaconda3/2020.11/etc/profile.d/conda.sh
 conda activate scEpilock
-python3 multi_task 'tuned_model'
+python3 multi_task 'tuned_model_2'
